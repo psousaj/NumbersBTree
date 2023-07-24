17fff30ca5d97cd3a0b359c45d64eefbda38cc18
José Sousa <josepsousa2012@gmail.com>
2023-07-23 22:39:04 -0300

    first commit

---
+++ src/main/java/br/psousaj/btree/NumbersBTreeApplication.java
@@ -0,0 +1,13 @@
+package br.psousaj.btree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumbersBTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumbersBTreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/psousaj/btree/NumbersBTreeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.psousaj.btree;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumbersBTreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
